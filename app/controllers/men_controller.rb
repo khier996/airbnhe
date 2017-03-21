67fcc976f2f1d512f4c60ab18abee266eacd896b
Antoine Deconinck <adeconinck.sina@gmail.com>
2017-03-21 11:52:07 +0800

    show-page

---
+++ app/controllers/men_controller.rb
@@ -0,0 +1,11 @@
+class MenController < ApplicationController
+  def home
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'men/home'
+
+  get 'men/show'
+
+  get 'men/new'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
