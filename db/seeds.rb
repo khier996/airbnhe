9a6092758be25c6ad488a72ed27f8af84fcd7c0d
Thomas Henderson <tbhenderson55@yahoo.com>
2017-03-21 13:03:26 +0800

    Starting search function

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Man.create({name: "Bob", height: 200, weight: 90, summary: "50 Shades of God", url: ,  birthdate:, description: "Love you long time" ,services: ["Love", "Passion", "Romance"] price: 2000})
+Man.create({name: "Bob", height: 200, weight: 90, summary: "50 Shades of God", url:"http://c8.alamy.com/comp/BHB8TA/adult-arab-arabian-arabian-peninsula-asia-dhofar-dishdash-dishdasha-BHB8TA.jpg" ,  birthdate: Time.now.to_datetime, description: "Love you long time" ,services: ["Love", "Passion", "Romance"], price: 2000})
+Man.create({name: "George", height: 200, weight: 90, summary: "50 Shades of God", url:"http://c8.alamy.com/comp/BHB8TA/adult-arab-arabian-arabian-peninsula-asia-dhofar-dishdash-dishdasha-BHB8TA.jpg" ,  birthdate: Time.now.to_datetime, description: "Love you long time" ,services: ["Love", "Passion", "Romance"], price: 2000})
+Man.create({name: "Max", height: 200, weight: 90, summary: "50 Shades of God", url:"http://c8.alamy.com/comp/BHB8TA/adult-arab-arabian-arabian-peninsula-asia-dhofar-dishdash-dishdasha-BHB8TA.jpg" ,  birthdate: Time.now.to_datetime, description: "Love you long time" ,services: ["Love", "Passion", "Romance"], price: 2000})
+Man.create({name: "Antoine", height: 200, weight: 90, summary: "50 Shades of God", url:"http://c8.alamy.com/comp/BHB8TA/adult-arab-arabian-arabian-peninsula-asia-dhofar-dishdash-dishdasha-BHB8TA.jpg" ,  birthdate: Time.now.to_datetime, description: "Love you long time" ,services: ["Love", "Passion", "Romance"], price: 2000})
