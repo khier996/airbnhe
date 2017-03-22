843c66ebefa9002b44465b79204cc4eb6e2e82ad
Antoine Deconinck <adeconinck.sina@gmail.com>
2017-03-22 12:01:07 +0800

    Show page v4

--- app/controllers/men_controller.rb
+++ app/controllers/men_controller.rb
@@ -1,5 +1,5 @@
 class MenController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def home
   end
