778945dba80cc8c409f745a0ba357a5fc3a250a5
Thomas Henderson <tbhenderson55@yahoo.com>
2017-03-23 13:15:27 +0800

    Profile page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   get 'men/index'
 
   root to: 'pages#home'
 
+  resources :users, only:[:show]
   resources :men
 
   mount Attachinary::Engine => "/attachinary"
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
