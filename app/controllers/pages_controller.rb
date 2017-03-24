46ec86251f8527226549373c5cb3ad85c95fe967
Patricia Wiesker <patricia@wiesker.de>
2017-03-24 17:05:25 +0800

    Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,19 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    # @current_choice = params[:bookings] || "all"
-    # if params[:bookings] == "confirmed"
-    #   @bookings = current_user.bookings.where(status: "confirmed")
+    @current_choice = params[:bookings] || "all"
+    if params[:bookings] == "confirmed"
+      @bookings = current_user.bookings.where(status: "confirmed")
 
-    # elsif params[:bookings] == "pending"
-    #   @bookings = current_user.bookings.where(status: "pending")
+    elsif params[:bookings] == "pending"
+      @bookings = current_user.bookings.where(status: "pending")
 
-    # elsif params[:bookings] == "rejected"
-    #   @bookings = current_user.bookings.where(status: "rejected")
+    elsif params[:bookings] == "rejected"
+      @bookings = current_user.bookings.where(status: "rejected")
 
-    # else
-    #   @bookings = current_user.bookings
-    # end
+    else
+      @bookings = current_user.bookings
+    end
 
     @bookings = current_user.bookings
     @requests = current_user.requests
